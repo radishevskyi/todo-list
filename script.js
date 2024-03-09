84978233f7a231b6880b1601087219e6e258ab43
radishevskyi <radishevskyi10@gmail.com>
2024-03-09 13:54:30 +0200

    update app

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const state = JSON.parse(localStorage.getItem("todoState")) || [
+let state = JSON.parse(localStorage.getItem("todoState")) || [
   { name: "Задача №1", checked: false },
   { name: "Задача №2", checked: true },
 ];
@@ -11,16 +11,35 @@ function saveStateToLocalStorage() {
   localStorage.setItem("todoState", JSON.stringify(state));
 }
 
-function createTask(task) {
+function createTask(task, index) {
   const li = document.createElement("li");
   li.className = "todo-list-item";
-  li.innerHTML = `<input class="input-checkbox" type="checkbox" ${task.checked ? 'checked' : ''} />
-                  <input class="input-value" value="${task.name}" type="text" />
-                  <button class="remove-btn">Remove</button>`;
+
+  const inputCheckbox = document.createElement("input");
+  inputCheckbox.type = "checkbox";
+  inputCheckbox.className = "input-checkbox";
+  inputCheckbox.checked = task.checked;
+
+  const inputText = document.createElement("input");
+  inputText.type = "text";
+  inputText.className = "input-value";
+  inputText.value = task.name;
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "Remove";
+  removeButton.className = "remove-btn";
+
+  li.appendChild(inputCheckbox);
+  li.appendChild(inputText);
+  li.appendChild(removeButton);
+
+  if (task.checked) {
+    inputText.classList.add("line-through");
+  }
 
   const removeBtn = li.querySelector(".remove-btn");
   removeBtn.addEventListener("click", () => {
-    removeTask(task);
+    removeTask(index);
   });
 
   const checkbox = li.querySelector(".input-checkbox");
@@ -36,13 +55,13 @@ function createTask(task) {
   return li;
 }
 
-function removeTask(taskToRemove) {
-  const index = state.findIndex(task => task === taskToRemove);
-  if (index !== -1) {
-    state.splice(index, 1);
-    saveStateToLocalStorage();
-    renderTasks();
-  }
+function removeTask(taskIndexToRemove) {
+    state = state.filter((_, index) => {
+    console.log(index, taskIndexToRemove);
+    return index !== taskIndexToRemove;
+  });
+  saveStateToLocalStorage();
+  renderTasks();
 }
 
 function updateTaskStatus(taskToUpdate, newStatus) {
@@ -58,19 +77,16 @@ function editTask(taskToEdit, newName) {
 
 function renderTasks() {
   list.innerHTML = "";
-  state.forEach((task) => {
-    list.appendChild(createTask(task));
+  state.forEach((task, index) => {
+    list.appendChild(createTask(task, index));
   });
 }
 
-state.forEach((task) => {
-  list.appendChild(createTask(task));
-});
+renderTasks();
 
 buttonSave.addEventListener("click", () => {
   state.push({ name: input.value, checked: false });
-  input.value = '';
+  input.value = "";
   saveStateToLocalStorage();
   renderTasks();
 });
-
